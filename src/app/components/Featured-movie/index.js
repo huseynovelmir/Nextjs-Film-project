61b16faef71b23e89e993d994ab77e140b410ff7
huseynovelmir <huseynovelmir789@gmail.com>
2023-05-16 20:53:01 +0400

    done

---
+++ src/app/components/Featured-movie/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FeaturedMovie() {
+  return (
+    <div>FeaturedMovie</div>
+  )
+}
+
+export default FeaturedMovie
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import global from "./styles/global.module.css"
 import { Inter } from 'next/font/google';
 import Header from   "./components/header/index";
 import Footer from "./components/footer/index";
+// import Head from "next/head";
 
 
 
@@ -19,6 +20,13 @@ const inter = Inter({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
+
+   
+      <head>
+        <link rel="icon" href="favicon.ico" />
+      </head>
+        
+
      
       <body className={global.container}>
         <Header />
