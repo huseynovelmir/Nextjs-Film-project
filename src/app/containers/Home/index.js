67b37a0e07c04386603d56457d58bdad35181811
huseynovelmir <huseynovelmir789@gmail.com>
2023-05-18 20:37:18 +0400

    done

--- src/app/containers/Home/index.js
+++ src/app/containers/Home/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Categories from '@/components/Categories'
 import Movies from "@/Mocks/Movies.json"
 import FeaturedMovie from '@/components/Featured-movie/index'
+import MoviesSection from '@/components/movies-section'
 
 function HomePage() {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[0]}/>
       <Categories movie={Movies.results.slice(0,5)}/>
+      <MoviesSection/>
     </div>
   )
 }
