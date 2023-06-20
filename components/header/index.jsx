431e0690550a31fd0133136c9834aed5fcedb687
huseynovelmir <huseynovelmir789@gmail.com>
2023-06-20 23:23:07 +0400

    DONE

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { BiCameraMovie } from 'react-icons/Bi'
+import {BiCameraMovie} from 'react-icons/Bi'
 import Link from 'next/link'
 import style from "./styles.module.css"
 
 function Header() {
   return (
     <header className={style.header}>
-      <Link href="/" className={style.logo}><BiCameraMovie />Find Movie</Link>
+      <Link href="/" className={style.logo}><BiCameraMovie/>Find Movie</Link>
     </header>
   )
 }
